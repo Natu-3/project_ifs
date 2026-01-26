ea80fcd3ba07cfb7949485d94d7941f1fff10510
Natu-3 <cyy3661@naver.com>
2026-01-26 10:19:07 +0900

    add springBoot settings

---
+++ backwork/src/test/java/com/example/backwork/BackworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
