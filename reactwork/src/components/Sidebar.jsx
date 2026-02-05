65852b10c270bee7a64d030b6ac6f27f0543dd1a
DESKTOP-K13K15N\human-10 <aa01077322224@gmail.com>
2026-02-05 10:56:03 +0900

    Revert "사이드바 중간 머지"
    This reverts commit c01c3e581aa61a1e078d9a14279ab679696e617f.

--- reactwork/src/components/Sidebar.jsx
+++ reactwork/src/components/Sidebar.jsx
@@ -137,10 +137,15 @@ export default function Sidebar() {
                     로그아웃
                 </button>
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 >>>>>>> parent of c01c3e5 (사이드바 중간 머지)
 =======
 >>>>>>> 18b87c1 (02.04.19:12 로그아웃 기능병합)
+=======
+=======
+>>>>>>> parent of c01c3e5 (사이드바 중간 머지)
+>>>>>>> e2f0487 (Revert "사이드바 중간 머지")
             </div>
         )}
         </div>
@@ -180,9 +185,12 @@ export default function Sidebar() {
             <div className="sidebar-list-wrapper">
 =======
 >>>>>>> parent of c01c3e5 (사이드바 중간 머지)
+<<<<<<< HEAD
 =======
             <div className="sidebar-list-wrapper">
 >>>>>>> 8f913f5 (사이드바 및 메인 note 사이드바 추가 add-btn 하단 고정 text공간 확보 26.02.05 09:29)
+=======
+>>>>>>> e2f0487 (Revert "사이드바 중간 머지")
             <ul className="sidebar-list">
                 {filteredPosts.map(post => (
                     <li
@@ -201,6 +209,7 @@ export default function Sidebar() {
                             }}
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 <<<<<<< HEAD
 >>>>>>> 3017083dd63962be2f59b2700e5299fb61d2bf12
@@ -209,6 +218,11 @@ export default function Sidebar() {
 >>>>>>> e2f048763801fac05ebc6281aa0b9cb89ccd8adc
 =======
 >>>>>>> ca0e1ad (사이드바 임시 저장)
+=======
+>>>>>>> 3017083dd63962be2f59b2700e5299fb61d2bf12
+=======
+>>>>>>> parent of c01c3e5 (사이드바 중간 머지)
+>>>>>>> e2f0487 (Revert "사이드바 중간 머지")
                         >
                             ★
                         </button>
@@ -226,9 +240,12 @@ export default function Sidebar() {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 <<<<<<< HEAD
 <<<<<<< HEAD
+=======
+>>>>>>> e2f0487 (Revert "사이드바 중간 머지")
                 </ul>
 =======
 >>>>>>> e2f048763801fac05ebc6281aa0b9cb89ccd8adc
@@ -252,12 +269,15 @@ export default function Sidebar() {
                 </button>
             </ul>
 >>>>>>> parent of c01c3e5 (사이드바 중간 머지)
+<<<<<<< HEAD
 =======
                 <button className="sidebar-add-btn" onClick={handleAddnewPost}>
                     +
                 </button>
             </div>
 >>>>>>> 8f913f5 (사이드바 및 메인 note 사이드바 추가 add-btn 하단 고정 text공간 확보 26.02.05 09:29)
+=======
+>>>>>>> e2f0487 (Revert "사이드바 중간 머지")
         </aside>
     )
 }
