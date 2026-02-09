b28cbe2ffd293cc5a37e1baedc7db1a7f93d643f
DESKTOP-PK960EB\human-24 <ckdwlsv@naver.com>
2026-02-09 15:21:43 +0900

    no message

--- backwork/src/main/java/com/example/backwork/auth/AuthController.java
+++ backwork/src/main/java/com/example/backwork/auth/AuthController.java
@@ -4,23 +4,35 @@ import com.example.backwork.member.CustomUserDetails;
 import com.example.backwork.member.SessionUser;
 import com.example.backwork.member.User;
 import com.example.backwork.member.dto.UserMeResponse;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
 public class AuthController {
+    //private final AuthenticationConfiguration authenticationConfiguration;
 
+    // private final AuthenticationManager authenticationManager;
     private final AuthService authService;
-
     @PostMapping("/login")
-    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request,HttpServletResponse response) {
-
+    public ResponseEntity<LoginResponse> login(
+            @RequestBody LoginRequest request,
+            HttpServletRequest httpRequest
+    ) {
+        User user = authService.login(request);
 
         HttpSession session = httpRequest.getSession(true);
         session.setAttribute("LOGIN_USER",
@@ -30,47 +42,33 @@ public class AuthController {
                         user.getAuth()
                 )
 
-                );
-
-        System.out.println("login request: " + request.getUserid());
-        LoginResult result = authService.login(request);
-        //Login 결과값 = 쿠키로 보내지 않을 키값까지 저장함
+        );
 
 
+//        ResponseCookie cookie = ResponseCookie.from("ACCESS_TOKEN", result.getAccessToken())
+//                .httpOnly(true)
+//                .path("/")
+//                .maxAge(60 * 30)
+//                .sameSite("Lax")
+//                .build();
 
-        //쿠키 값 설정 부분
-        ResponseCookie cookie = ResponseCookie.from("ACCESS_TOKEN", result.getAccessToken())
-                .httpOnly(true)
-                .secure(false)
-                .path("/")
-                .maxAge(60 * 30)
-                .sameSite("Lax")
-                .build();
-        response.addHeader("Set-Cookie", cookie.toString());
+        // response.addHeader("Set-Cookie", cookie.toString());
 
-        User user = result.getUser();
-        String dev = result.getDevToken();
-        System.out.println(dev);
+        //User user = result.getUser();
 
-        // 리턴 - 로컬용 ID / UserID / 사용자 권한 / 임시토큰
         return ResponseEntity.ok(
                 new LoginResponse(
                         user.getId(),
                         user.getUserid(),
-                        user.getAuth(),
-                        dev
+                        user.getAuth()
+                        // result.getDevToken()
                 )
         );
     }
 
-      //  return ResponseEntity.ok(authService.login(request));
-
-
-
     @PostMapping("/signup")
-    public ResponseEntity<?> signup(@RequestBody SignupRequest request){
-        System.out.println("Sign up cleared");
-                return ResponseEntity.ok(authService.singup(request));
+    public ResponseEntity<?> signup(@RequestBody SignupRequest request) {
+        return ResponseEntity.ok(authService.singup(request));
     }
 //    jwt 기반 구현했던것
 //    @GetMapping("/me")
@@ -105,21 +103,6 @@ public class AuthController {
         if (user == null) {
             return ResponseEntity.status(401).build();
         }
-
-
-        CustomUserDetails user =
-                (CustomUserDetails) authentication.getPrincipal();
-
-        assert user != null;
-
-            return ResponseEntity.status(401).build();
-        }
-
-        SessionUser user = (SessionUser) session.getAttribute("LOGIN_USER");
-        if (user == null) {
-            return ResponseEntity.status(401).build();
-        }
-
 
         return ResponseEntity.ok(
                 new UserMeResponse(
