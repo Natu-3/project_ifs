89a96c958b0e66b924cb2624a60e3b56d434d072
DESKTOP-PK960EB\human-24 <ckdwlsv@naver.com>
2026-02-09 14:07:57 +0900

    벡엔드 캘린더의 Schedule(스케줄) 작업 26.02.09 -entity -dto -repository -service -controller

---
+++ backwork/src/main/java/com/example/backwork/schedule/dto/ScheduleResponse.java
@@ -0,0 +1,22 @@
+package com.example.backwork.schedule.dto;
+
+import com.example.backwork.schedule.entity.Schedule;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class ScheduleResponse {
+    private Long id;
+    private String title;
+    private String content;
+    private LocalDateTime startAt;
+    private LocalDateTime endAt;
+    public ScheduleResponse(Schedule schedule) {
+        this.id = schedule.getId();
+        this.title = schedule.getTitle();
+        this.content = schedule.getContent();
+        this.startAt = schedule.getStartAt();
+        this.endAt = schedule.getEndAt();
+    }
+}
