cd5f8e29a29acd5925e005a39b6e6577d0ed3d1d
DESKTOP-PK960EB\human-24 <ckdwlsv@naver.com>
2026-02-11 18:04:19 +0900

    스케줄 Api 수정 26.02.11 18:04

--- reactwork/src/api/scheduleApi.js
+++ reactwork/src/api/scheduleApi.js
@@ -6,3 +6,21 @@ export const getMonthSchedules = (year, month) => {
         withCredentials: true,
     })
 }
+
+export const createSchedule = (scheduleData) => {
+    return axios.post("/schedule", scheduleData, {
+        withCredentials: true,
+    });
+};
+
+export const updateSchedule = (scheduleId, scheduleData) => {
+    return axios.put(`/schedule/${scheduleId}`, scheduleData, {
+        withCredentials: true,
+    });
+};
+
+export const deleteSchedule = (scheduleId) => {
+    return axios.delete(`/schedule/${scheduleId}`, {
+        withCredentials: true,
+    });
+};
